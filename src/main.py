367d763be321a8f902b0db34308de86cbfa05c6b
rapha <onanaraphaelnathan@gmail.com>
2026-02-07 14:22:51 -0500

    setup done

---
+++ src/main.py
@@ -0,0 +1,27 @@
+# from presidio_analyzer import AnalyzerEngine
+
+# # Set up the engine, loads the NLP module (spaCy model by default) and other PII recognizers
+# analyzer = AnalyzerEngine()
+
+# # Call analyzer to get results
+# results = analyzer.analyze(text="My phone number is 212-555-5555",
+#                            entities=["PHONE_NUMBER"],
+#                            language='en')
+# print(results)
+
+
+
+from PIL import Image
+from presidio_analyzer import Pattern, PatternRecognizer
+from presidio_image_redactor import ImageRedactorEngine
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+# Standard images
+engine = ImageRedactorEngine()
+
+image = Image.open(Path("sample_data/test_image2.png"))
+
+redacted_image = engine.redact(image, (155, 11, 103))
+redacted_image.show()
